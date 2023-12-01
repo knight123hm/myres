9c3514c0ea06729043fbe83889b66aea7a80c57f
knight123hm <930824365@qq.com>
2023-12-01 18:46:51 +0800

    首次的代码提交

---
+++ files.c
@@ -0,0 +1,15 @@
+i#include<stdio.h>
+
+int main(void)
+{
+printf("abc ");
+printf("abc ");
+printf("abc ");
+printf("abc ");
+printf("abc ");
+printf("abc ");
+printf("abc ");
+printf("abc ");
+printf("abc ");
+
+}
